95944488a9d200e2a4da1ae679961f8e8bd2b460
rado_poghossian <rado.poghossian0398@gmail.com>
2019-06-13 22:30:57 +0400

    changed admin.hs

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const {create} = require('../controllers/cart.controller')
+const { create, index } = require('../controllers/cart.controller')
 
 /* GET users listing. */
-router.post('/cart',create);
+router.post('/cart', create);
+router.get('/cart', index);
 
 module.exports = router;
